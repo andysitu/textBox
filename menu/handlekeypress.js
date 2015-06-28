ba10a729a6b1b406f2b3bb1aff484a976d2507bd
Andy <and.situ@gmail.com>
2015-06-27 20:19:29 -0700

    initial keypress function created
    * very basic. empties textbox

---
+++ menu/handlekeypress.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+	var subBut = document.getElementById("submitButton");
+	var textBox = document.getElementById("textBox");
+
+
+// Functions not inputted for if text is submitted
+	subBut.onclick = function() {
+		textBox.value = "";
+	}
+
+// if enter key is pressed, then it simulates as if submit button is clicked.
+	textBox.onkeypress = function(e) {
+		if (e.keycode === 13)
+			subBut.click();
+};
