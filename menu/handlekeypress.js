20934fe1cd0fade218b06c35787ea73d308f61bf
Andy <and.situ@gmail.com>
2015-06-28 13:36:40 -0700

    separated click/press functions from onload

--- menu/handlekeypress.js
+++ menu/handlekeypress.js
@@ -2,15 +2,29 @@ window.onload = function() {
 	var subBut = document.getElementById("submitButton");
 	var textBox = document.getElementById("textBox");
 
+	display("Hello");
 
 // Functions not inputted for if text is submitted
-	subBut.onclick = function() {
-		textBox.value = "";
-	}
+	subBut.onclick = clicky;
 
 // if enter key is pressed, then it simulates as if submit button is clicked.
-	textBox.onkeypress = function(e) {
-		if (e.keycode === 13)
-			subBut.click();
-	};
+	textBox.onkeypress = pressKey;
+
 }
+
+function clicky() {
+	var textBox = document.getElementById("textBox");
+
+	var input = textBox.value.toUpperCase().toString();
+	display(input);
+	textBox.value = "";
+}
+
+function pressKey(e) {
+	var subBut = document.getElementById("submitButton");
+
+	if (e.keycode === 13) {
+		subBut.click();
+		return false;
+	}
+};
