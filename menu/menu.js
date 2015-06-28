8be8062a60127b9299b865864cc35454bc64165c
Andy <and.situ@gmail.com>
2015-06-28 13:36:26 -0700

    Changed display function to change value, not innerHTML

--- menu/menu.js
+++ menu/menu.js
@@ -1,8 +1,20 @@
+// Function to display any message given to it to the textbox
 function display(msg) {
 	var here = document.getElementById("area");
-	here.innerHTML = here.innerHTML + " \n" +  msg;
+	here.value = here.value + " \n" +  msg;
 };
 
 const displayScreens = {
-	mainMenu: "Hello, type a, b, or c"
+	mainMenu: "Hello, type \"hp\" to see health of player. ",
+	playerStatus: player1["health"],
+};
+
+const responseStor = {
+	hp(){
+		display(displayScreens["playerStatus"]);
+	},
+}
+
+function response(value) {
+	responseStor[value]();
 };
