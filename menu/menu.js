d92baefa8c14fd0c0b708bb82606a4fdcad1bd2c
Andy <and.situ@gmail.com>
2015-06-28 21:04:42 -0700

    Added fight option for monsters (very basic)
    * fight generates monster stats
    * changes to random level (1-3) and random health of monster based on
    player and monster level

--- menu/menu.js
+++ menu/menu.js
@@ -1,30 +1,35 @@
-// Function to display any message given to it to the textbox
-function display(msg) {
-	var here = document.getElementById("area");
-	here.value = here.value +  msg + "\n";
-};
+
 
 const displayScreens = {
-	startMenu: "Hello, type \"hp\" to see health of player. ",
+	startMenu: "Hello, type \"hp\" to see health of player, \"status\" to see status of player",
 	playerStatus: player1["health"],
 };
 
+// obj storing functions for response by input of player. 
 const responseStor = {
 	HP(){
 		display("You have " + player1["health"] + "/" + player1["maxHealth"] + " HP");
-	},
+	 },
 	STATUS(){
-		var str = ""
+		var str = "Current status:"
 
-		player1.each(function(value, key){
-			str = str + value
+		each(player1, function(value, key) {
+			str = str + " " + value + " " + key;
 		});
-	},
 
+		display(str);
+	 },
+	FIGHT() {
+		monster["fightStatus"] = true;
+		monster["generateMonster"]();
+		monster["genMonMsg"]();
+	 },
+
+// response runs the functions on responseStor directly by having the values be the name of the methods.
 	response(value) {
 		if (this[value])
 			this[value]();
 		else
 			display("The command " + value + " doesn't exist. Please try again.")
-	},
+	 },
 }
--- world/monster.js
+++ world/monster.js
@@ -2,3 +2,18 @@
 //
 // Might give it abilities and skills later
 
+var monster = {
+	fightStatus: false, // player can only attack monster when true
+	level: 1,
+	health: 1,
+	mana: 1,
+
+	generateMonster() {
+
+		this["level"] = Math.ceil(Math.random() * 4);
+		this["health"] = player1["level"] * this["level"] + player1["level"] * this["level"] * Math.ceil(Math.random() * 0.5);
+		this["mana"] = player1["level"] * this["level"] + player1["level"] * this["level"] * Math.ceil(Math.random() * 0.5);
+	},
+
+	genMonMsg() { display("You've encounted a monster! Level " + this["level"] + " and HP " + this["health"] + "."); },
+}
