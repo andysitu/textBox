c264b7c90455234cf25965c86f50aedb1dfbf59d
Andy <and.situ@gmail.com>
2015-07-01 19:39:21 -0700

    misc
    * added commas to status message
    * fight command checks if there is already a monster generated (by
    checking fightstatus from monster)

--- menu/menu.js
+++ menu/menu.js
@@ -1,5 +1,4 @@
 
-
 const displayScreens = {
 	startMenu: "Type \"hp\" to see health of player, \"status\" to see status of player, \"fight\" to fight a monster",
 	playerStatus: player1["health"],
@@ -8,20 +7,25 @@ const displayScreens = {
 // obj storing functions for response by input of player. 
 const responseStor = {
 	HP(){
-		display("You have " + player1["health"] + "/" + player1["maxHealth"] + " HP");
+		display("You have " + player1["health"] + "/" + player1["max health"] + " HP");
 	 },
 	STATUS(){
 		var str = "Current status:";
 		each(player1, function(value, key) {
 			if (typeof value !== "function")
-				str = str + " " + value + " " + key;
+				str = str + " " + value + " " + key + ",";
 		});
+		str = str.slice(0, -1);
 		display(str);
 	 },
 	FIGHT() {
-		monster["fightStatus"] = true;
-		monster["generateMonster"]();
-		monster["genMonMsg"]();
+		if (!monster["fightStatus"]) {
+			monster["fightStatus"] = true;
+			monster["generateMonster"]();
+			monster["genMonMsg"]();
+		} else {
+			display("You're already fight a monster.");
+		}
 	 },
 	 ATTACK() {
 	 	// ATTACK calculates player damge & returns the damage into rewarder method 
--- world/player.js
+++ world/player.js
@@ -2,7 +2,7 @@
 
 var player1 = {
 	health: 50,
-	maxHealth: 50,
+	"max health": 50,
 	level: 1,
 	exp: 0,
 	gold: 0,
@@ -13,7 +13,7 @@ var player1 = {
 	},
 	levelUp() {
 		if (this.exp >= this.level * this.level * 5) {
-			this.health = this.maxHealth = this.level * 35;
+			this.health = this["max health"] = this.level * 35;
 			this.level++;
 			this.exp = 0;
 			display("Congraultions! You're now level " + this.level);
